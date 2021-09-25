9cd8114ea49730be70cd252e1dd2638f4720f137
terraaksara <muh.fir.firdaus@gmail.com>
2021-09-25 13:56:16 +0700

    Update Student

--- get_data_user/get_user_uskelas.php
+++ get_data_user/get_user_uskelas.php
@@ -2,10 +2,10 @@
 include "../database_connection/connection.php";
  
 $uskabkot = $_GET['uskabkot'];
-$uskelas  = mysqli_query($koneksi, "SELECT id_uskelas, nama_kelas FROM link_mentor_kelas WHERE id_uskabkot='$uskabkot' order by nama_kelas");
+$uskelas  = mysqli_query($koneksi, "SELECT id_uskelas, nama_uskelas FROM link_mentor_kelas WHERE id_uskabkot='$uskabkot' order by nama_kelas");
  
 echo "<option>-- Pilih kelas --</option>";
 while($k = mysqli_fetch_array($uskelas)){
-echo "<option value=\"".$k['id_uskelas']."\">".$k['nama_kelas']."</option>\n";
+echo "<option value=\"".$k['id_uskelas']."\">".$k['nama_uskelas']."</option>\n";
 }
 ?>
