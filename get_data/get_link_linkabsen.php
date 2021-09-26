99546e1dad7eb25390843f69185bc02e0bdc254a
terraaksara <muh.fir.firdaus@gmail.com>
2021-09-26 16:40:44 +0700

    Update Lesson

--- get_data/get_link_linkabsen.php
+++ get_data/get_link_linkabsen.php
@@ -5,7 +5,7 @@ $pertemuan = $_GET['pertemuan'];
 $link  = mysqli_query($koneksi, "SELECT id_link, nama_link FROM link_absen WHERE id_pertemuan ='$pertemuan' order by nama_link");
  
 "<option>-- Lihat Link --</option>";
-while($k = mysqli_fetch_array($pertemuan)){
+while($k = mysqli_fetch_array($link)){
 echo "<option value=\"".$k['id_link']."\">".$k['nama_link']."</option>\n";
 }
 ?>
