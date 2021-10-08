c4cf22aae43c307f92f8b725a9ef0d23a9a4cb02
terraaksara <muh.fir.firdaus@gmail.com>
2021-10-08 14:27:43 +0700

    Update Administrasi

--- get_data_user/get_user_absen.php
+++ get_data_user/get_user_absen.php
@@ -4,7 +4,7 @@ include "../database_connection/connection.php";
 $pertemuan = $_GET['pertemuan'];
 $absen  = mysqli_query($koneksi, "SELECT id_link, nama_link FROM link_mentor_absen WHERE id_pertemuan='$pertemuan' order by nama_link");
  
-echo "<option>-- Pilih kelas --</option>";
+"<option>-- Pilih kelas --</option>";
 while($k = mysqli_fetch_array($absen)){
 echo "<option value=\"".$k['id_link']."\">".$k['nama_link']."</option>\n";
 }
