06200d6cdaf1ce0351ba287a3d3abad4de95a2c9
terraaksara <muh.fir.firdaus@gmail.com>
2021-10-08 14:16:03 +0700

    Update Administrasi

--- get_data_user/get_user_pertemuan.php
+++ get_data_user/get_user_pertemuan.php
@@ -2,7 +2,7 @@
 include "../database_connection/connection.php";
  
 $uskelas = $_GET['uskelas'];
-$pertemuan  = mysqli_query($koneksi, "SELECT id_pertemuan, nama_pertemuan FROM link_pertemuan_mentor WHERE id_uskelas='$uskelas' order by nama_link_kelas");
+$pertemuan  = mysqli_query($koneksi, "SELECT id_pertemuan, nama_pertemuan FROM link_pertemuan_mentor WHERE id_uskelas='$uskelas' order by nama_pertemuan");
  
 echo "<option>-- Pilih Pertemuan --</option>";
 while($k = mysqli_fetch_array($pertemuan)){
