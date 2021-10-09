d51ca8b3882ac031491fe394042c519876ec1bb7
terraaksara <muh.fir.firdaus@gmail.com>
2021-10-09 17:09:33 +0700

    Supervisior Page

--- get_data_panitia/get_data_link.php
+++ get_data_panitia/get_data_link.php
@@ -2,7 +2,7 @@
 include "../database_connection/connection.php";
  
 $jenjang = $_GET['jenjang'];
-$link  = mysqli_query($koneksi, "SELECT id_link, nama_link FROM jenjang_absen WHERE id_jenjang='$jenjang' order by nama_link");
+$link  = mysqli_query($koneksi, "SELECT id_link, nama_link FROM panitia_absen WHERE id_jenjang='$jenjang' order by nama_link");
  
 "<option>-- Pilih kelas --</option>";
 while($k = mysqli_fetch_array($link)){
